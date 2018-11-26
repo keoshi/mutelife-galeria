7420ecfcc087abe8a4fd9903a38e4578810f2a59
Filipe Varela <keoshi@keoshi.com>
2018-11-26 14:36:47 +0000

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# mutelife Galeria
+
+Automatically generate beautiful galleries!
+
+[mutelife](https://mutelife.com/)
+
+Props to:
+Jorge Costa
+Jon Surrell
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: mutelife Galeria
+ * Plugin URI: https://mutelife.com/
+ * Description: Automatically generate beautiful galleries with Gutenberg blocks!
+ * Version: 0.1.0
+ * Author: Filipe Varela
+ *
+ * @package mutelife-galeria
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+include 'galeria/index.php';
